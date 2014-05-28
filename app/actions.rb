71fc1bfcd512e1abd61e726ae177241715488313
billytianlan <billylan22@gmail.com>
2014-05-28 04:16:34 +0000

    Updated CSS signup and login

--- app/actions.rb
+++ app/actions.rb
@@ -7,6 +7,20 @@ get '/new' do
   erb :'users/new'
 end
 
+post '/new' do
+  @current_user = User.new(
+  username: params[:username],
+  password: params[:password]
+  )
+# binding.pry
+  if @current_user.save
+    session[:user_id] = @current_user.id
+    redirect "/photos"
+  else
+    erb :'users/signup'
+  end
+end
+
 get '/login' do
   erb :'users/login'
 end
